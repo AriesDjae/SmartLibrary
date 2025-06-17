576ec09a00a62d50bebabcb2cfb957e8e123f618
auliraa <lyerarhm@gmail.com>
2025-06-17 15:54:52 +0700

    database

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,38 +1,107 @@
 //framework untuk buat server di Node.js
 const express = require('express');
+const {ObjectId} = require('mongodb');
+const {connectToDb, getDb} = require('./db');
 
-//untuk konek dan berinteraksi dengan MongoDB
-const mongoose = require('mongoose');
-
-//baca file .env
+// //baca file .env
 require('dotenv').config();
 
-//Inisialisasi aplikasi Express
+// // //untuk konek dan berinteraksi dengan MongoDB
+// // const mongoose = require('mongoose');
+
+// //Inisialisasi aplikasi Express
 const app = express();
-const port = 3000;
 
-// // Tentukan port (ambil dari env kalau ada, kalau nggak default ke 3000)
-// const PORT = process.env.PORT || 3000;
+// // //port untuk server
+const port = process.env.PORT || 3000;
 
-// Middleware untuk parsing request body dalam format JSON
+// // Middleware untuk parsing request body dalam format JSON
 app.use(express.json());
 
-// ===================== KONEKSI KE MONGODB =====================
 
-// Koneksi ke MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+// //db connection
+let db;
+connectToDb((err) => {
+    if(!err){
+        app.listen(port, () => {
+          console.log(`Server berjalan di http://localhost:${port}`);
+        })
+        db = getDb()
+    }
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Connection error:', err));
 
-// ===================== ROUTE CONTOH =====================
-app.get('/', (req, res) => {
-  res.send("Hello World I'am Node.js!");
+
+// // ===================== ROUTES =====================
+
+app.get('/api/books', (req, res) => {
+
+    //current page
+    const page = req.query.p || 0 
+    const booksPerPage  = 3
+
+    let books = []
+
+    db.collection('books')
+        .find() //cursor to Array forEach
+        .sort({author: 1}) //1 for ascending, -1 for descending
+        .skip(page * booksPerPage)
+        .limit(booksPerPage)
+        .forEach(book => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+        })
+        .catch(() => {
+            res.status(500).json({error: 'Could not fetch the documents'})
+        })
 });
 
-// ===================== JALANKAN SERVER =====================
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+app.get('/api/books/:id', (req, res) =>{
+    const bookId = req.params.id;
+    db.collection('books')
+        .findOne({_id: bookId})
+        .then(doc => {
+            res.status(200).json(doc)
+        })
+        .catch(err => {
+            res.status(500).json({error : 'Could not fetch the documents'})
+        })
+})
+
+app.post('/api/books', (req, res) => {
+    const book = req.body
+
+    db.collection('books')
+        .insertOne(book)
+        .then(result => {
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({err : 'could not create a new document'})
+        })
+})
+
+//delete by id
+app.delete('/api/books/:id', (req, res) =>{
+    const bookId = req.params.id;
+    db.collection('books')
+        .deleteOne({_id: bookId})
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({error : 'Could not delete the documents'})
+        })  
+})
+
+app.patch('/api/books/:id', (req, res) =>{
+    const updates = req.body
+    const bookId = req.params.id;
+    db.collection('books')
+        .updateOne({_id: bookId}, {$set: updates})
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({error : 'Could not update the documents'})
+        }) 
+})
