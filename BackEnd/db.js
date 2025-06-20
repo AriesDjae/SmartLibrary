1f69e826784be8f86b019f8367b2e7b7f53aefb1
Aleanzho <imaduddinibnulhakim@gmail.com>
2025-06-20 18:14:04 +0700

    Update db.js

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,22 +1,26 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-let dbConnection
+let dbConnection;
 
-let uri = process.env.MONGODB_URI;
+// Default MongoDB URI for localhost
+const defaultUri = 'mongodb://localhost:3000/smartlibrary';
+
+// Use environment variable if available, otherwise use default
+const uri = process.env.MONGODB_URI || defaultUri;
 
 module.exports = {
-    connectToDb : (cb) => {
+    connectToDb: (cb) => {
         MongoClient.connect(uri)
-        .then((client) => {
-            dbConnection = client.db()
-            return cb()
-        })
-        .catch(err => {
-            console.log(err)
-            return cb(err)
-        })
+            .then((client) => {
+                dbConnection = client.db();
+                console.log('Successfully connected to MongoDB.');
+                return cb();
+            })
+            .catch(err => {
+                console.error('Error connecting to MongoDB:', err);
+                return cb(err);
+            });
     },
     getDb: () => dbConnection
-
-}
+};
